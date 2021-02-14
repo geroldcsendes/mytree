ed79c5faf3b2fbb023837ce7f97a8fb85a0843eb
gerold-csendes-epam <gerold_csendes@epam.com>
2021-02-14 20:55:03 +0100

    restructure tests

--- test_tree.py
+++ test_tree.py
@@ -15,26 +15,52 @@ class TestTree(unittest.TestCase):
         path_test = pathlib.Path(path_test).resolve()
 
         # create result tree
-        result = tree_maker(input_path=str(path_test), write_md=True)
+        result = tree_maker(
+            input_path=str(path_test), write_md=True)
+
+        # print test result
+        res_path = "testdir/test_base_res.md"
+        res_path = pathlib.Path(res_path).resolve()
+
+        # uncomment when experimenting
+        # with open(res_path, "w+", encoding='utf-8') as writer:
+        #     writer.write(result)
 
         # expected tree
-        path_expected = "testdir/test1_exp.md"
+        path_expected = "testdir/test_base_exp.md"
         path_expected = pathlib.Path(path_expected).resolve()
 
         with open(path_expected, "r", encoding="utf-8") as reader:
             expected = reader.read()
 
-        # print test result
-        test_res_path = "testdir/test1_res.md"
-        test_res_path = pathlib.Path(test_res_path).resolve()
-
-        # with open(test_res_path, "w+", encoding='utf-8') as writer:
-        #     writer.write(result)
-
         self.assertEqual(result, expected)
 
     def test_csv_exclusion(self):
-        return True
+
+        # define test directory
+        path_test = "testdir/test1"
+        path_test = pathlib.Path(path_test).resolve()
+
+        # create result tree
+        result = tree_maker(
+            input_path=str(path_test), write_md=True, exclude_ext=".csv")
+
+        # save
+        res_path = "testdir/test_excl-csv_res.md"
+        res_path = pathlib.Path(res_path).resolve()
+
+        # uncomment when experimenting
+        # with open(res_path, "w+", encoding='utf-8') as writer:
+        #     writer.write(result)
+        
+        # expected tree
+        path_expected = "testdir/test_excl-csv_exp.md"
+        path_expected = pathlib.Path(path_expected).resolve()
+
+        with open(path_expected, "r", encoding="utf-8") as reader:
+            expected = reader.read()
+
+        self.assertEqual(result, expected)
 
     def test_init_exclusion(self):
         return True
